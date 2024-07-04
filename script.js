3895aed8f99d3d00ed9973358b72e0eda447a141
Kiat Kai <58962961+kiatkai96@users.noreply.github.com>
2024-07-04 08:40:16 +0800

    Make  commits to edit education and removed collection. Did minor edits to display

---
+++ script.js
@@ -0,0 +1,48 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + " ]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// init Isotope
+var $grid = $(".collection-list").isotope({
+  // options
+});
+// filter items on button click
+$(".filter-button-group").on("click", "button", function () {
+  var filterValue = $(this).attr("data-filter");
+  resetFilterBtns();
+  $(this).addClass("active-filter-btn");
+  $grid.isotope({ filter: filterValue });
+});
+
+// This creates the function when we clicked on any of the filter group withint the collection-list, the
+// buttons will reset
+var filterBtns = $(".filter-button-group").find("button");
+function resetFilterBtns() {
+  filterBtns.each(function () {
+    $(this).removeClass("active-filter-btn");
+  });
+}
